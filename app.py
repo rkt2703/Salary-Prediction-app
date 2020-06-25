7a2c6681eb82cdd4bc824d9e452418b8495edcc3
rkt2703 <35970266+rkt2703@users.noreply.github.com>
2020-06-25 22:12:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__)
+myapp = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
